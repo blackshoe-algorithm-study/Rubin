9a7231aad229b2a362968e7b359170a42b055a49
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-26 17:08:32 +0900

    0326

--- march_week4/프린터_큐.java
+++ march_week4/프린터_큐.java
@@ -9,9 +9,61 @@ public class 프린터_큐 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Map<String, Integer> map = new HashMap<>();
-        
+        PriorityQueue<Integer> ranks;
+        Deque<Integer> q;
 
-        Deque deque = new ArrayDeque<>();
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        while(t-->0){
+            st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int targetIdx = Integer.parseInt(st.nextToken());
+
+            String[] brReadLine = br.readLine().split(" ");
+
+            ranks = new PriorityQueue<>(Collections.reverseOrder());
+
+            ranks.addAll(Stream.of(brReadLine)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList()));
+
+
+            q = Stream.of(brReadLine)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toCollection(ArrayDeque::new));
+
+            int rank = 1;
+            while(!q.isEmpty() && !ranks.isEmpty()){
+
+                //주어진 q에서 첫번째 요소 제거
+                int num = q.remove();
+
+                //만약 첫 번째 요소가 우선순위가 제일 높다면
+                if (num == ranks.peek()){
+
+                    if(targetIdx == 0){
+
+                        bw.write(rank+"\n");
+                        break;
+                    }
+
+                    //우선순위 제일 높은 것 제거, 자동으로 최대 heapify됨
+                    ranks.remove();
+                    rank++;
+                }
+
+                //다시 맨 뒤에 삽입
+                q.addLast(num);
+
+                targetIdx--;
+                if (targetIdx < 0) {
+                    targetIdx = q.size() - 1;
+                }
+
+            }
+        }
+        bw.flush();
     }
 }
