8fabbea7a4a4e34629948121af06e7e339b11e74
Subin <70842920+bin-pro@users.noreply.github.com>
2024-07-14 11:00:19 +0900

    투포인터

---
+++ 실전테스트3/주몽2.java
@@ -0,0 +1,44 @@
+package 실전테스트3;
+
+import java.io.*;
+import java.util.*;
+public class 주몽2 {
+    static int N, M, sol;
+    static int[] m;
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        m = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            m[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(m);
+
+        sol = 0;
+        int left = 0;
+        int right = N-1;
+
+        //투 포인터 시작
+        while(left < right) {
+            int sum = m[left] + m[right];
+            if(sum == M){
+                sol++;
+                left++;
+                right--;
+            }else if(sum < M){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        bw.write(String.valueOf(sol));
+        bw.flush();
+    }
+
+}
