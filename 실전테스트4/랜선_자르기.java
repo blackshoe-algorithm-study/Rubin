7633886bbb4267de9f79f0dac9446704599927b1
Subin <70842920+bin-pro@users.noreply.github.com>
2024-08-01 13:19:17 +0900

    Solve 랜선자르기

--- 실전테스트4/랜선_자르기.java
+++ 실전테스트4/랜선_자르기.java
@@ -6,63 +6,39 @@ public class 랜선_자르기 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //정해진 것 : 잘라진 랜선 총 갯수보다 커야하며, 자를 수 있는 최대 길이
-
-        // 1. 랜선을 다 자른 뒤 N보다 커야함
-        // 2. 자른 각각의 랜선 길이는 최대여야함.
-        // -> 자른 각각의 랜선 길이가 최대가 되도록 하며 자른 갯수가 N보다 큰 답을 출력
-        // -> 브루트포스, 모든 경우의 수
-
-        /*
-        4 11
-802
-743
-457
-539
-         */
-
-        int K, N;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()), base = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[K];
-        for(int i = 0; i < K; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        long max = 0L;
+        long[] arr = new long[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(arr[i], max);
         }
-        int div = 1;
+        long left = 1, right = max;
+        long mid;
+        long maxLen = 0;
 
-        int max = Integer.MIN_VALUE;
+        while(left<=right){
+            mid = (left+right)/2;
 
-        //count = 막대 갯수
-        //div = 막대 나누는 수
+            long quote, curr = 0;
 
-        Arrays.sort(arr);
-        //System.out.println(Arrays.toString(arr));
-        while(true) {
-            int chunk = 0;
-            div++;
-            boolean cFlag = false;
-
-            for(int i = 0; i < K; i++){
-                chunk += arr[i]/div;
-
-                if(chunk >= N && i < K-1) {
-                    //System.out.println("chunk :" + chunk + " i : " + i);
-                    cFlag = true;
-                    break;
-                }
+            for(int i = 0; i < N; i++){
+                quote = arr[i]/mid;
+                curr += quote;
             }
-            if(cFlag) continue;
 
-            if(chunk >= N){
-                max = Math.max(max, div);
-            } else if(chunk < N && max < div){
-              break;
+            if(curr >= base){
+                left = mid + 1;
+                maxLen = Math.max(maxLen, mid);
+            }else if(curr < base){
+                right = mid - 1;
             }
 
         }
-        bw.write(max + "\n");
+
+        bw.write(maxLen + "\n");
         bw.flush();
     }
 }
